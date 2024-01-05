792811edf988957fd2c06d0d92e6d679e84a5c8e
Taranpreet Natt <taranpreetnatt@Taranpreets-MBP.lan>
2024-01-04 20:20:02 -0800

    added docker login

--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -3,9 +3,3 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
-
-test("should display github status", async () => {
-  const { getByTestId } = render(<App />);
-  await waitForElement(() => getByTestId("status"));
-  expect(getByTestId("status")).toBeTruthy();
-});
